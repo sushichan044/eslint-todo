12962cd461d0ebd7a3da9db18727b15b2c4fdac8
sushichan044 <mail@sushichan.live>
2025-02-06 01:06:28 +0900

    feat: add minimum cli

---
+++ src/cli.ts
@@ -0,0 +1,40 @@
+import { defineCommand, runMain } from "citty";
+
+import type { Options } from "./options";
+
+import { generateESLintTodo } from ".";
+
+const cli = defineCommand({
+  args: {
+    cwd: {
+      description: "Current working directory",
+      required: false,
+      type: "string",
+      valueHint: "path",
+    },
+    "todo-file": {
+      description: "ESLint todo file name",
+      required: false,
+      type: "string",
+      valueHint: "filename",
+    },
+  },
+  meta: {
+    description:
+      "Generate ESLint todo file and temporally suppress ESLint errors!",
+    name: "@sushichan044/eslint-todo/cli",
+    version: "0.0.0",
+  },
+  async run({ args }) {
+    const options = {
+      cwd: args.cwd,
+      todoFile: args["todo-file"],
+    } satisfies Options;
+
+    await generateESLintTodo(options);
+  },
+});
+
+export const run = async (argv: string[]): Promise<void> => {
+  await runMain(cli, { rawArgs: argv });
+};
