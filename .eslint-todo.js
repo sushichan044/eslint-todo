46c480cd6ae5d3e66e166c1aa497c7c8e5ef4302
sushichan044 <mail@sushichan.live>
2025-02-06 01:20:11 +0900

    chore: generate todo file

---
+++ .eslint-todo.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+/**
+ * Auto generated file by eslint-todo. DO NOT EDIT MANUALLY.
+ */
+
+const eslintTodo = {
+  "no-undef": {
+    "autoFix": false,
+    "files": [
+      "bin/eslint-todo.mjs",
+      "bin/eslint-todo.mjs",
+      "bin/eslint-todo.mjs"
+    ]
+  }
+};
+
+export default eslintTodo;
