d307048a2ce67caccf8221247def3065890fbadc
sushichan044 <mail@sushichan.live>
2025-02-09 02:04:25 +0900

    ci: add test for resolveTodoFilePath

---
+++ src/utils/path.test.ts
@@ -0,0 +1,49 @@
+import { describe, expect, it } from "vitest";
+
+import type { Options } from "../options";
+
+import { resolveTodoFilePath } from "./path";
+
+describe("resolveTodoFilePath", () => {
+  it("should resolve the correct absolute and relative paths", () => {
+    const options: Options = {
+      cwd: "/home/sushi/workspace",
+      todoFile: ".eslint-todo.js",
+    };
+
+    const result = resolveTodoFilePath(options);
+
+    expect(result).toStrictEqual({
+      absolute: "/home/sushi/workspace/.eslint-todo.js",
+      relative: ".eslint-todo.js",
+    });
+  });
+
+  it("should handle nested todoFile paths", () => {
+    const options: Options = {
+      cwd: "/home/sushi/workspace",
+      todoFile: "nested/.eslint-todo.js",
+    };
+
+    const result = resolveTodoFilePath(options);
+
+    expect(result).toStrictEqual({
+      absolute: "/home/sushi/workspace/nested/.eslint-todo.js",
+      relative: "nested/.eslint-todo.js",
+    });
+  });
+
+  it("should handle absolute todoFile paths", () => {
+    const options: Options = {
+      cwd: "/home/sushi/workspace",
+      todoFile: "/home/sushi/.eslint-todo.js",
+    };
+
+    const result = resolveTodoFilePath(options);
+
+    expect(result).toStrictEqual({
+      absolute: "/home/sushi/.eslint-todo.js",
+      relative: "../.eslint-todo.js",
+    });
+  });
+});
