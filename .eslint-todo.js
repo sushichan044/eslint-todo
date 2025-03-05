4b0c9b3908738dd8ce4e5387512338683ba05288
sushichan044 <mail@sushichan.live>
2025-03-05 21:07:20 +0900

    chore: regen todo file

--- .eslint-todo.js
+++ .eslint-todo.js
@@ -8,14 +8,5 @@ export default {
     version: 2
   },
 
-  todo: {
-    "unicorn/no-array-reduce": {
-      autoFix: false,
-
-      violations: {
-        "src/todofile/v1.ts": 3,
-        "src/todofile/v2.ts": 1
-      }
-    }
-  }
+  todo: {}
 };
