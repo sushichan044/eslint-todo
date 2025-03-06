8bc8fc5be6bc068e89d8850634a9dc61a682c9f0
sushichan044 <mail@sushichan.live>
2025-03-06 23:48:18 +0900

    refactor: use json like object internally

--- src/cli/action/list.ts
+++ src/cli/action/list.ts
@@ -7,13 +7,21 @@ import { TodoModuleV2Handler } from "../../todofile/v2";
 import { defineAction } from "./index";
 
 export const listAction = defineAction(async ({ core, logger }) => {
+  // v1 module should be upgraded to v2 at the entry point of CLI
   const todoModule = getTodoModuleV2(await core.readTodoModule());
 
-  if (todoModule === false) {
+  if (!todoModule) {
     throw new Error("Unsupported todo file version");
   }
 
-  const todoTable = generateTodoTable(todoModule);
+  const todoTable = generateTodoInfo(todoModule).map((row) => {
+    return [
+      row.rule,
+      row.violations.files,
+      row.violations.total,
+      row.autoFix ? "✔" : "",
+    ];
+  });
 
   if (todoTable.length === 0) {
     logger.success("No rules found in the todo file! Good job!");
@@ -23,29 +31,26 @@ export const listAction = defineAction(async ({ core, logger }) => {
   const tableData = [["Rule", "Files", "Violations", "AutoFix"], ...todoTable];
 
   logger.log.raw(table(tableData));
-
   return;
 });
 
-const getTodoModuleV2 = (module: TodoModuleLike): false | TodoModuleV2 => {
-  if (TodoModuleV2Handler.isVersion(module)) {
-    return module;
-  }
-
-  return false;
+const getTodoModuleV2 = (module: TodoModuleLike): TodoModuleV2 | null => {
+  return TodoModuleV2Handler.isVersion(module) ? module : null;
 };
 
-type TableFormat = [
-  ruleId: string,
-  violatedFiles: number,
-  totalViolations: number,
-  autoFixableEmoji: string,
-];
+type TodoInfo = {
+  autoFix: boolean;
+  rule: string;
+  violations: {
+    files: number;
+    total: number;
+  };
+};
 
-const generateTodoTable = (todoModule: TodoModuleV2): TableFormat[] => {
-  const tableData: TableFormat[] = [];
+const generateTodoInfo = (todoModule: TodoModuleV2): TodoInfo[] => {
+  const todoInfo: TodoInfo[] = [];
 
-  for (const [ruleId, { autoFix, violations }] of Object.entries(
+  for (const [rule, { autoFix, violations }] of Object.entries(
     todoModule.todo,
   )) {
     const violatedFiles = Object.keys(violations).length;
@@ -54,10 +59,15 @@ const generateTodoTable = (todoModule: TodoModuleV2): TableFormat[] => {
       0,
     );
 
-    const autoFixEmoji = autoFix ? "✓" : "";
-
-    tableData.push([ruleId, violatedFiles, totalViolations, autoFixEmoji]);
+    todoInfo.push({
+      autoFix,
+      rule,
+      violations: {
+        files: violatedFiles,
+        total: totalViolations,
+      },
+    });
   }
 
-  return tableData;
+  return todoInfo;
 };
