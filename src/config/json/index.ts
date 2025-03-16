abbb7f6812b074268b6024f9b690ce552c3adf0f
sushichan044 <mail@sushichan.live>
2025-03-17 01:53:44 +0900

    refactor: avoid ts-expect-error

--- src/config/json/index.ts
+++ src/config/json/index.ts
@@ -7,5 +7,4 @@ import type { UserConfig } from "../config";
  */
 // 普通の typia.json.schemas を使うと OpenAPI docs 用の json schema が生成されるが、
 // 今回欲しいのは普通の json schema なので HACK ではあるが typia.llm.schema を使う。
-// @ts-expect-error 想定されていない使い方なので型エラーが出ているが、単に json schema を生成するだけなら問題なので無視する
-export const UserConfigJsonSchema = typia.llm.schema<UserConfig, "3.1">();
+export const UserConfigJsonSchema = typia.llm.schema<UserConfig, "3.1">({});
