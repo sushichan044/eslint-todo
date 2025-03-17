a9a63dc332ea2dbec24c973cb5e2a27a1f92730a
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-03-18 00:48:58 +0900

    chore: add hook to cli action

--- src/cli/action/index.ts
+++ src/cli/action/index.ts
@@ -1,38 +1,62 @@
 import type { Remote } from "comlink";
 import type { ConsolaInstance } from "consola";
+import type { Hookable, HookCallback, HookKeys } from "hookable";
+
+import { createHooks } from "hookable";
 
 import type { Config } from "../../config";
 import type { RemoteESLintTodoCore } from "../../remote/core";
-import type { IsNever, MaybePromise } from "../../utils/types";
+import type { DeepPartial, IsNever, MaybePromise } from "../../utils/types";
 
 import { launchRemoteESLintTodoCore } from "../../remote/client";
 
-type ActionAPI = {
+type HookHandlers<Hooks extends Record<string, HookCallback>> = Record<
+  HookKeys<Hooks>,
+  Hooks[HookKeys<Hooks>]
+>;
+
+type ActionAPI<
+  Hooks extends Record<string, HookCallback> = Record<string, never>,
+> = {
   config: Config;
   core: Remote<RemoteESLintTodoCore>;
+  hooks: Hookable<Hooks>;
   logger: ConsolaInstance;
 };
 
 /**
  * @package
  */
-export type CLIAction<Input = unknown, Return = unknown> =
+export type CLIAction<
+  Input = unknown,
+  Return = unknown,
+  Hooks extends Record<string, HookCallback> = Record<string, never>,
+> =
   IsNever<Input> extends true
-    ? (api: ActionAPI) => MaybePromise<Return>
-    : (api: ActionAPI, input: Input) => MaybePromise<Return>;
+    ? (api: ActionAPI<Hooks>) => MaybePromise<Return>
+    : (api: ActionAPI<Hooks>, input: Input) => MaybePromise<Return>;
 
 /**
  * Define a CLI action.
  *
  * @package
  */
-export const defineAction = <Input = never, Return = unknown>(
-  action: CLIAction<Input, Return>,
-) => action;
+export const defineAction = <
+  Input = never,
+  Return = unknown,
+  Hooks extends Record<string, HookCallback> = Record<string, never>,
+>(
+  action: CLIAction<Input, Return, Hooks>,
+) => {
+  return action;
+};
 
-type ActionRunnerOptions = {
+type ActionRunnerOptions<
+  Hooks extends Record<string, HookCallback> = Record<string, never>,
+> = {
   config: Config;
   consola: ConsolaInstance;
+  hooks?: DeepPartial<HookHandlers<Hooks>>;
 };
 
 /**
@@ -42,32 +66,45 @@ type ActionRunnerOptions = {
  * @param options Options for running the action
  * @returns A function that can be called with input to execute the action
  */
-export function prepareAction<Input = never, Return = unknown>(
-  action: CLIAction<Input, Return>,
-  options: ActionRunnerOptions,
+export function prepareAction<
+  Input = never,
+  Return = unknown,
+  Hooks extends Record<string, HookCallback> = Record<string, never>,
+>(
+  action: CLIAction<Input, Return, Hooks>,
+  options: ActionRunnerOptions<Hooks>,
 ): IsNever<Input> extends true
   ? () => Promise<Return>
   : (input: Input) => Promise<Return> {
-  const { config, consola } = options;
+  const hooks = createHooks<Hooks>();
+
+  if (options.hooks) {
+    // @ts-expect-error - TypeScript reports hookHandlers has number properties, but it's actually only has string properties
+    hooks.addHooks(options.hooks);
+  }
 
   const executor = async (input?: Input) => {
     // initialize remote ESLintTodoCore
     const remoteService = launchRemoteESLintTodoCore();
-    const remoteCore = await new remoteService.RemoteESLintTodoCore(config);
+    const remoteCore = await new remoteService.RemoteESLintTodoCore(
+      options.config,
+    );
 
     const actionApi = {
-      config,
+      config: options.config,
       core: remoteCore,
-      logger: consola,
-    } satisfies ActionAPI;
+      hooks,
+      logger: options.consola,
+    } satisfies ActionAPI<Hooks>;
 
     try {
-      if (input === undefined) {
-        return await (action as CLIAction<never, Return>)(actionApi);
-      }
-      return await action(actionApi, input);
+      const result =
+        input === undefined
+          ? await (action as CLIAction<never, Return, Hooks>)(actionApi)
+          : await action(actionApi, input);
+
+      return result;
     } catch (error) {
-      consola.error(error);
       throw error;
     } finally {
       await remoteService.terminate();
