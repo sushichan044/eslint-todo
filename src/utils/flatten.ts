bff834c2b04c46fe323b2903ed6f1868c74e7afd
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-06-24 00:03:48 +0900

    feat: define utility type to flatten object

---
+++ src/utils/flatten.ts
@@ -0,0 +1,21 @@
+export type FlattenObject<T> = {
+  [K in FlattenKeys<T>]: K extends `${infer P}.${infer S}`
+    ? P extends keyof T
+      ? T[P] extends Record<PropertyKey, unknown>
+        ? S extends keyof T[P]
+          ? T[P][S]
+          : never
+        : never
+      : never
+    : K extends keyof T
+      ? T[K]
+      : never;
+};
+
+type FlattenKeys<T, K extends keyof T = keyof T> = K extends string
+  ? T[K] extends readonly unknown[]
+    ? K
+    : T[K] extends Record<PropertyKey, unknown>
+      ? `${K}.${FlattenKeys<T[K]>}`
+      : K
+  : never;
