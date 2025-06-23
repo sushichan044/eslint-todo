02921ddf34e3232ca191bc37f50aa886d297430f
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-06-24 04:17:26 +0900

    build: use tsdown with unbundle mode

---
+++ tsdown.config.ts
@@ -0,0 +1,35 @@
+import NodeExternals from "rollup-plugin-node-externals";
+import { defineConfig } from "tsdown";
+
+import { typiaRolldown } from "./typia-plugin";
+
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entry: [
+    "./src/index.ts",
+    "./src/config/index.ts",
+    "./src/eslint/index.ts",
+    "./src/cli/index.ts",
+    "./src/mcp/index.ts",
+
+    // Worker threads are depends on relative file structure.
+    // So we need to include it explicitly to avoid any bundling issue or tree-shaking issue.
+    "./src/worker/**/*.ts",
+    "!./src/worker/**/*.test.ts",
+  ],
+  format: "esm",
+  minify: "dce-only",
+  outDir: "dist",
+  outExtensions: () => {
+    return {
+      js: ".mjs",
+    };
+  },
+  plugins: [typiaRolldown, NodeExternals()],
+  publint: true,
+  sourcemap: false,
+  treeshake: true,
+  unbundle: true,
+  unused: true,
+});
--- typia-plugin.ts
+++ typia-plugin.ts
@@ -1,9 +1,11 @@
 import type { Options } from "@ryoppippi/unplugin-typia";
 
-import UnpluginTypiaRollUp from "@ryoppippi/unplugin-typia/rollup";
+import UnpluginTypiaRolldown from "@ryoppippi/unplugin-typia/rolldown";
 import UnpluginTypiaVite from "@ryoppippi/unplugin-typia/vite";
 
 const typiaOptions: Options | undefined = undefined;
 
-export const typiaRollup = UnpluginTypiaRollUp(typiaOptions);
+// we don't want to install rolldown explicitly.
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+export const typiaRolldown = UnpluginTypiaRolldown(typiaOptions);
 export const typiaVite = UnpluginTypiaVite(typiaOptions);
