5d10be1f100301ecc843f95802f2fc8bc2657bf5
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-06-24 11:15:49 +0900

    chore(devDeps): add rolldown to avoid type error (not wanted)

--- typia-plugin.ts
+++ typia-plugin.ts
@@ -5,7 +5,5 @@ import UnpluginTypiaVite from "@ryoppippi/unplugin-typia/vite";
 
 const typiaOptions: Options | undefined = undefined;
 
-// we don't want to install rolldown explicitly.
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export const typiaRolldown = UnpluginTypiaRolldown(typiaOptions);
 export const typiaVite = UnpluginTypiaVite(typiaOptions);
