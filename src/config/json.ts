1aba6b75514c0133871cff25d4d42c0f48fcaed6
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-07-27 13:16:29 +0900

    fix: migrate into typia 9.6+ compatible type args

--- src/config/json.ts
+++ src/config/json.ts
@@ -4,4 +4,8 @@ import type { UserConfig } from "./config";
 
 // 普通の typia.json.schemas を使うと OpenAPI docs 用の json schema が生成されるが、
 // 今回欲しいのは普通の json schema なので HACK ではあるが typia.llm.schema を使う。
-export const UserConfigJsonSchema = typia.llm.schema<UserConfig, "3.1">({});
+export const UserConfigJsonSchema = typia.llm.schema<
+  UserConfig,
+  "3.1",
+  { constraint: true; reference: false }
+>({});
