e0b02a8e78a6dd8929a4b061391ed548af538d10
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-07-30 00:16:20 +0900

    move file

--- src/operation/strategies/types.ts
+++
@@ -1,8 +0,0 @@
-import type { CorrectModeConfig } from "../../config/config";
-import type { MaybePromise } from "../../utils/types";
-import type { RuleViolationInfo } from "../select-rule";
-
-export type CandidateCollectionStrategy = (
-  violations: RuleViolationInfo[],
-  config: CorrectModeConfig,
-) => MaybePromise<RuleViolationInfo[]>;
