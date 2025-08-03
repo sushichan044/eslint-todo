4e7df43db138997ee31f179b14cfeb309056ea69
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-08-04 01:50:37 +0900

    fix typedef

--- src/operation/select-rule/filters/types.ts
+++ src/operation/select-rule/filters/types.ts
@@ -2,7 +2,20 @@ import type { Config } from "../../../config/config";
 import type { MaybePromise } from "../../../utils/types";
 import type { RuleViolationInfo } from "../index";
 
-export type ViolationFilteringStrategy = (
-  info: RuleViolationInfo,
-  config: Config,
-) => MaybePromise<RuleViolationInfo>;
+/**
+ * @package
+ */
+export type ViolationFilteringStrategy = {
+  name: string;
+  run: (
+    info: RuleViolationInfo,
+    context: ViolationFilteringStrategyContext,
+  ) => MaybePromise<RuleViolationInfo>;
+};
+
+/**
+ * @package
+ */
+export type ViolationFilteringStrategyContext = {
+  config: Config;
+};
