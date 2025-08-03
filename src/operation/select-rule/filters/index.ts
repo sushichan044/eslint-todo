7010e1f5fcd640f536a39b1cc078abfecd62c1f1
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-08-04 01:51:29 +0900

    memoize module graph

--- src/operation/select-rule/filters/index.ts
+++ src/operation/select-rule/filters/index.ts
@@ -1,40 +1,40 @@
 import type { RuleViolationInfo } from "..";
 import type { Config } from "../../../config/config";
-import type { ViolationFilteringStrategy } from "./types";
+import type { ViolationFilteringStrategyContext } from "./types";
 
+import { resolveModules } from "../../../utils/module-graph";
 import { applyTransforms } from "../../../utils/transform";
-import { importGraphBasedStrategy } from "./import-graph";
+import { ImportGraphBasedStrategy } from "./import-graph";
 import { includeExcludeFilter } from "./include-exclude";
-
-const getViolationFilterStrategies = (
-  config: Config,
-): ViolationFilteringStrategy[] => {
-  const filters: ViolationFilteringStrategy[] = [];
-
-  if (config.correct.strategy.type === "import-graph") {
-    filters.push(importGraphBasedStrategy);
-  }
-
-  // this should be the last filter
-  filters.push(includeExcludeFilter);
-
-  return filters;
-};
 
 export const applyViolationFilters = async (
   infos: RuleViolationInfo[],
   config: Config,
 ): Promise<RuleViolationInfo[]> => {
-  const strategies = getViolationFilterStrategies(config);
+  const strategies = [includeExcludeFilter];
+  if (config.correct.strategy.type === "import-graph") {
+    const resolvedModules = await resolveModules(
+      config.correct.strategy.entrypoints,
+      {
+        baseDir: config.root,
+      },
+    );
+    if (resolvedModules.error == null) {
+      strategies.unshift(
+        new ImportGraphBasedStrategy({
+          reachableFiles: new Set(resolvedModules.modules.map((m) => m.source)),
+        }),
+      );
+    }
+  }
 
   const result: RuleViolationInfo[] = [];
 
   for (const info of infos) {
-    const transformed = await applyTransforms<RuleViolationInfo, Config>(
-      info,
-      strategies,
-      { context: config },
-    );
+    const transformed = await applyTransforms<
+      RuleViolationInfo,
+      ViolationFilteringStrategyContext
+    >(info, strategies, { context: { config } });
 
     if (transformed.error != null) {
       // Log the error but continue processing other violations
