56d10d942e4a274fff84801d06803ea5f5c48ba3
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2026-01-02 15:21:05 +0900

    chore: bump build tools

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,32 +1,29 @@
 import { defineConfig } from "tsdown";
 
+import { typiaRolldown } from "./typia-plugin";
+
 export default defineConfig({
-  attw: { profile: "esmOnly" },
-  clean: true,
+  attw: { level: "error", profile: "esm-only" },
+    clean: true,
   dts: true,
   entry: [
     "./src/index.ts",
     "./src/config/index.ts",
     "./src/eslint/index.ts",
     "./src/cli/index.ts",
-    "./src/mcp/index.ts",
 
     // Worker threads depend on relative file structure.
     // So we need to include it explicitly to avoid any bundling issue or tree-shaking issue.
     "./src/worker/**/*.ts",
     "!./src/worker/**/*.test.ts",
   ],
+  fixedExtension:true,
   format: "esm",
-  fromVite: true,
   minify: "dce-only",
   nodeProtocol: true,
   onSuccess: "pnpm run build:json-schema",
   outDir: "dist",
-  outExtensions: () => {
-    return {
-      js: ".mjs",
-    };
-  },
+  plugins: [typiaRolldown],
   publint: true,
   sourcemap: false,
   treeshake: true,
--- typia-plugin.ts
+++ typia-plugin.ts
@@ -1,7 +1,9 @@
 import type { Options } from "@ryoppippi/unplugin-typia";
 
+import UnpluginTypiaRolldown from "@ryoppippi/unplugin-typia/rolldown";
 import UnpluginTypiaVite from "@ryoppippi/unplugin-typia/vite";
 
 const typiaOptions: Options | undefined = undefined;
 
 export const typiaVite = UnpluginTypiaVite(typiaOptions);
+export const typiaRolldown = UnpluginTypiaRolldown(typiaOptions);
