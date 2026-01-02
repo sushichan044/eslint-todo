d8e025e1417db2701bd3d33c5c97b6fbcc71db29
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2026-01-02 14:46:28 +0900

    chore: use oxfmt

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-import prettier from "@virtual-live-lab/prettier-config";
-
-/**
- * @type {import("prettier").Config}
- */
-export default {
-  ...prettier,
-  plugins: [...(prettier.plugins ?? []), "@prettier/plugin-oxc"],
-};
