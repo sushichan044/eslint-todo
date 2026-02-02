10a3dd0d0ef4872b89776617fe44d459d9bc6352
Kentaro Suzuki <71284054+sushichan044@users.noreply.github.com>
2026-02-02 22:45:29 +0900

    chore(deps): update devDeps (#413)
    * chore: update devDeps
    
    * remove typegen
    
    * we no longer using codegen
    
    * simplify

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,63 +1,54 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="./eslint-typegen.d.ts" />
-
 import type { ESLint } from "eslint";
 
 import ts from "@virtual-live-lab/eslint-config/presets/ts";
 import vitest from "@vitest/eslint-plugin";
 import importAccess from "eslint-plugin-import-access/flat-config";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
-import typegen from "eslint-typegen";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 
 import eslintConfigTodo from "./dist/eslint/index.mjs";
 
-export default typegen(
-  defineConfig(
-    globalIgnores(["src/generated/**"]),
-    {
-      extends: [ts],
+export default defineConfig(
+  ts,
+  {
+    extends: [eslintPluginUnicorn.configs.recommended],
+    name: "@repo/eslint-config/unicorn",
+    rules: {
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "kebabCase",
+        },
+      ],
+      "unicorn/no-null": "off",
     },
-    {
-      extends: [eslintPluginUnicorn.configs.recommended],
-      name: "@repo/eslint-config/unicorn",
-      rules: {
-        "unicorn/filename-case": [
-          "error",
-          {
-            case: "kebabCase",
-          },
-        ],
-        "unicorn/no-null": "off",
-      },
+  },
+  {
+    extends: [vitest.configs.recommended],
+    files: ["**/*.test.ts", "**/*.spec.ts"],
+    rules: {
+      "vitest/consistent-test-filename": "error",
     },
-    {
-      extends: [vitest.configs.recommended],
-      files: ["**/*.test.ts", "**/*.spec.ts"],
-      rules: {
-        "vitest/consistent-test-filename": "error",
-      },
+  },
+  {
+    files: ["**/*.ts"],
+    plugins: {
+      // Plugin の型が typescript-eslint ベースなので合わない
+      "import-access": importAccess as ESLint.Plugin,
     },
-    {
-      files: ["**/*.ts"],
-      plugins: {
-        // Plugin の型が typescript-eslint ベースなので合わない
-        "import-access": importAccess as ESLint.Plugin,
-      },
-      rules: {
-        "import-access/jsdoc": "error",
-      },
+    rules: {
+      "import-access/jsdoc": "error",
     },
-    {
-      files: ["bin/eslint-todo.mjs"],
-      languageOptions: {
-        globals: globals.nodeBuiltin,
-      },
-      rules: {
-        "unicorn/filename-case": "off",
-      },
+  },
+  {
+    files: ["bin/eslint-todo.mjs"],
+    languageOptions: {
+      globals: globals.nodeBuiltin,
     },
-    await eslintConfigTodo(),
-  ),
+    rules: {
+      "unicorn/filename-case": "off",
+    },
+  },
+  await eslintConfigTodo(),
 );
